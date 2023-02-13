8a039b0cde850149dc7994d9adcdfe05930d5d52
softwaretestlab <softwaretestlab@hotmail.com>
2023-02-13 13:52:47 +1100

    JIRA-007

---
+++ src/test/java/GitLearning/TC01_NavigateToWebsite.java
@@ -0,0 +1,12 @@
+package GitLearning;
+
+import org.junit.Test;
+
+public class TC01_NavigateToWebsite {
+
+    @Test
+    public void Nav(){
+        System.out.println("Navigating to website");
+    }
+
+}

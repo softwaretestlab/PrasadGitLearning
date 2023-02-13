90b48874be66f9b6243f9ab4a857fbc690d5fb96
softwaretestlab <softwaretestlab@hotmail.com>
2023-02-13 13:53:46 +1100

    comments

--- src/test/java/GitLearning/TC01_NavigateToWebsite.java
+++ src/test/java/GitLearning/TC01_NavigateToWebsite.java
@@ -3,7 +3,7 @@ package GitLearning;
 import org.junit.Test;
 
 public class TC01_NavigateToWebsite {
-
+// JIRA-007 user story
     @Test
     public void Nav(){
         System.out.println("Navigating to website");
